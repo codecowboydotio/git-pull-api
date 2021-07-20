2dbd7b0dbfc91fee7782a579987c597b02bb070f
codecowboydotio <scott@vankalken.org>
2021-07-21 09:12:39 +1000

    wsgi testing

---
+++ app.py
@@ -0,0 +1,36 @@
+from flask import Flask, jsonify, request, redirect
+from flasgger import Swagger
+from flasgger.utils import swag_from, validate
+from jsonschema import ValidationError
+import git
+from git import Repo
+from pathlib import Path
+
+app = Flask(__name__)
+swagger = Swagger(app)
+
+@app.route ('/')
+def root():
+    return redirect("/apidocs", code=302)
+@app.route('/pull', methods=['POST'])
+@swag_from('unit-flask-git.yml')
+def pull():
+    result_data = request.get_data()
+    print(result_data)
+    data = request.get_json(force=True)
+    repo = data['repo']
+    branch = data['branch']
+    dest = data['dest']
+
+    path_exists = Path(dest).is_dir()
+    if path_exists == True:
+      git_repo = Repo(dest)
+      origin = git_repo.remotes.origin 
+      origin.pull()
+    else:
+      Repo.clone_from(repo, dest)    
+
+    return jsonify(repo=repo, branch=branch, dest=dest)
+
+if __name__ == "__main__":
+  app.run(host='0.0.0.0')
