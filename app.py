575873d1ea3423bb9d684f1efb940d24dafe241e
root <root@ip-10-100-1-76.ap-southeast-2.compute.internal>
2021-07-25 14:36:08 +0000

    updated app

--- app.py
+++ app.py
@@ -18,21 +18,9 @@ def pull():
     result_data = request.get_data()
     print(result_data)
     data = request.get_json(force=True)
-    print ("\n\n")
-    html_url = data['repository']['html_url']
-    message = data['commits'][0]['message']
-    print (html_url)
-    print (message)
-    print ("\n\n")
-  ##  repo = data['repository']['html_url']
-  ##  branch = 'main'
-  ##  dest = data['commits'][0]['message']
     repo = data['repo']
     branch = data['branch']
     dest   = data['dest']
-  #  config_url = data['config_url']
-  #  key = data['key']
-  #  val = data['val']
 
     path_exists = Path(dest).is_dir()
     if path_exists == True:
@@ -43,7 +31,6 @@ def pull():
     else:
       Repo.clone_from(repo, dest)    
 
-    #return jsonify(repo=repo, branch=branch, dest=dest, config_url=config_url, key=key, val=val)
     return jsonify(repo=repo, branch=branch, dest=dest)
 
 @app.route('/info', methods=['GET'])
